5d4366b74ea8ffb6bff39bb75db3bc2ac47e0fd9
Aleksander <aleksander@Aleksanders-iMac.local>
2017-05-30 21:22:58 -0700

    added headers to lifestyle cells

--- Introduction/LifestyleTableViewCell.swift
+++ Introduction/LifestyleTableViewCell.swift
@@ -11,10 +11,15 @@ import UIKit
 class LifestyleTableViewCell: UITableViewCell {
     
     @IBOutlet fileprivate weak var collectionView: UICollectionView!
+    @IBOutlet weak var headerLabel: UILabel!
     
 }
 
 extension LifestyleTableViewCell {
+    
+    override func awakeFromNib() {
+        
+    }
     
     func setCollectionViewDataSourceDelegate<D: UICollectionViewDataSource & UICollectionViewDelegate>(_ dataSourceDelegate: D, forRow row: Int) {
         
@@ -23,10 +28,22 @@ extension LifestyleTableViewCell {
         collectionView.tag = row
         collectionView.setContentOffset(collectionView.contentOffset, animated:false) // Stops collection view if it was scrolling.
         collectionView.reloadData()
+        
+        switch(collectionView.tag){
+        case 0:
+            headerLabel.text = "Hiking"
+        case 1:
+            headerLabel.text = "Other"
+        case 2:
+            headerLabel.text = "Sport"
+        default:
+            break
+        }
     }
     
     var collectionViewOffset: CGFloat {
         set { collectionView.contentOffset.x = newValue }
         get { return collectionView.contentOffset.x }
     }
+    
 }
